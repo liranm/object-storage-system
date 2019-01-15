5c1eca072a14f2a271a45363ee3f02dde0823072
Liran Mery <liran135@gmail.com>
2019-01-15 16:01:42 +0200

    install mongoose and add Files collection

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const File = require('./models/File');
+
+const db_url = 'mongodb://localhost/files';
+
+mongoose.connect(db_url, { useNewUrlParser: true });
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', async (req, res) => {
+    const filesCount = await File.countDocuments();
+    
+    res.send(`Total Files: ${filesCount}`);
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
