dfc3b326f85f5e7b1d4995f7b4970abcc8e7ad8a
Liran Mery <liran135@gmail.com>
2019-01-17 12:33:59 +0200

    install multer

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const multer = require('multer');
+
+const upload = multer();
 
 const File = require('./models/File');
 
@@ -9,6 +12,11 @@ mongoose.connect(db_url, { useNewUrlParser: true });
 
 const app = express();
 const port = 3000;
+
+app.post('/', upload.single('file_path') , async (req, res) => {
+    console.log(req.file);
+    res.json(req.file);
+});
 
 app.get('/', async (req, res) => {
     const filesCount = await File.countDocuments();
