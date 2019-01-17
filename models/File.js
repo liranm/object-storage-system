b921b8201a7d792a90704641425bae956719e95d
Liran Mery <liran135@gmail.com>
2019-01-17 11:08:36 +0200

    add updatedAt to File schema

--- models/File.js
+++ models/File.js
@@ -3,11 +3,16 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
  
-const FileSchema = new Schema({
-  author: ObjectId,
-  title: String,
-  body: String,
-  date: Date
-});
+const FileSchema = new Schema(
+    {
+        author: ObjectId,
+        title: String,
+        body: String,
+        date: Date
+    },
+    {
+        timestamps: true
+    }
+);
 
 module.exports = mongoose.model('File', FileSchema);
