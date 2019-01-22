95c21ef39a89167e206f8b2b503ad8edf205c98a
Liran Mery <liran135@gmail.com>
2019-01-22 11:44:42 +0200

    add modify file mode

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const multer = require('multer');
 const fs = require('fs');
-const uniqid = require('uniqid'); 
+const uniqid = require('uniqid');
+const bodyParser = require('body-parser');
 
 const upload = multer({ dest: 'uploads/' })
 
@@ -17,6 +18,41 @@ mongoose.connect(db_url, {
 
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.put('/modify', async (req, res) => {
+    if(!req.body.mode || !req.body.filename) {
+        return res.status(400).send('Missing required data.');
+    }
+
+    switch(req.body.mode) {
+        case 'public':
+        case 'private':
+            const { filename, mode } = req.body;
+            const fileDoc = await File.findOne({ $or: [ { private_url: filename }, { public_url: filename } ] });
+
+            if(!fileDoc) {
+                return res.status(404).send('File not found.');
+            }
+
+            if(req.header('X-AUTH') !== fileDoc.owner) {
+                return res.status(401).send('Authentication failed.');
+            }
+
+            if(mode === fileDoc.mode) {
+                return res.status(200).send(`Mode is already ${mode}`);
+            }
+
+            fileDoc.set({ mode });
+
+            await fileDoc.save();
+
+            return res.status(200).send(`Mode updated to ${mode}`);
+        default:
+            return res.status(400).send('Invalid mode.');
+    }
+});
 
 app.post('/', upload.single('file_path') , async (req, res) => {
     const db = await mongoose.connection.db;
