f9e2564fe1eb40e4cbb4241b730c13087c9d8830
Liran Mery <liran135@gmail.com>
2019-01-30 16:00:28 +0200

    update form to get real user input

--- client/src/components/UploadForm.js
+++ client/src/components/UploadForm.js
@@ -3,7 +3,13 @@ import React, { Component } from 'react';
 class UploadForm extends Component {
     constructor(props) {
         super(props);
-        this.fileInput = React.createRef();
+        this.state = {
+            file_path: '',
+            public_url: '',
+            owner: '',
+            mode: 'public',
+            fileInputKey: Date.now()
+        };
     }
 
     handleSubmit = (event) => {
@@ -11,26 +17,77 @@ class UploadForm extends Component {
         
         const formData = new FormData();
 
-        formData.append('public_url', '/abcd/test.txt');
-        formData.append('owner', '123qwea');
-        formData.append('mode', 'private');
-        formData.append('file_path', this.fileInput.current.files[0]);
+        formData.append('public_url', this.state.public_url);
+        formData.append('owner', this.state.owner);
+        formData.append('mode', this.state.mode);
+        formData.append('file_path', this.state.file_path);
 
         fetch('http://localhost:3000/', {
             method: 'POST',
             body: formData
         })
-        .then(response => response.text())
-        .catch(error => console.error('Error:', error))
-        .then(response => console.log('Success:', response));
+            .then(response => response.text())
+            .catch(error => console.error('Error:', error))
+            .then(response => {
+                console.log('Success:', response);
+                this.setState({
+                    file_path: '',
+                    public_url: '',
+                    owner: '',
+                    mode: 'public',
+                    fileInputKey: Date.now()
+                });
+            });
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.files ? 
+                event.target.files[0] : 
+                event.target.value 
+        });
     }
     
     render() {
         return (
             <form onSubmit={this.handleSubmit}>
                 <label>
-                    Upload File:
-                    <input type="file" ref={this.fileInput} />
+                    Upload file:
+                    <input
+                        key={this.state.fileInputKey}
+                        type="file"
+                        onChange={this.handleChange}
+                        name="file_path"
+                    />
+                </label>
+                <label>
+                    File path:
+                    <input
+                        type="text"
+                        onChange={this.handleChange}
+                        value={this.state.public_url}
+                        name="public_url"
+                    />
+                </label>
+                <label>
+                    Owner key:
+                    <input
+                        type="text"
+                        onChange={this.handleChange}
+                        value={this.state.owner}
+                        name="owner"
+                    />
+                </label>
+                <label>
+                    File mode:
+                    <select
+                        onChange={this.handleChange}
+                        value={this.state.mode}
+                        name="mode"
+                    >
+                        <option value="public">Public</option>
+                        <option value="private">Private</option>
+                    </select>
                 </label>
                 <button type="submit">Submit</button>
             </form>
